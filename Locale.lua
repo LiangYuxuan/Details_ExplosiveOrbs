1d331080c179d96e9f4d2f8c65668230e358755b
LiangYuxuan <lyx@lyx.es>
2019-11-26 10:32:18 +0800

    misc: update Locale.lua to support more language

--- Locale.lua
+++ Locale.lua
@@ -13,7 +13,23 @@ Engine.L = setmetatable(L, {
 })
 
 if locale == 'zhCN' then
-    --@localization(locale="zhCN", format="lua_additive_table")@
+--@localization(locale="zhCN", format="lua_additive_table", handle-unlocalized="comment")@
 elseif locale == 'zhTW' then
-    --@localization(locale="zhTW", format="lua_additive_table")@
+--@localization(locale="zhTW", format="lua_additive_table", handle-unlocalized="comment")@
+elseif locale == 'deDE' then
+--@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="comment")@
+elseif locale == 'esES' then
+--@localization(locale="esES", format="lua_additive_table", handle-unlocalized="comment")@
+elseif locale == 'esMX' then
+--@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment")@
+elseif locale == 'frFR' then
+--@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="comment")@
+elseif locale == 'itIT' then
+--@localization(locale="itIT", format="lua_additive_table", handle-unlocalized="comment")@
+elseif locale == 'koKR' then
+--@localization(locale="koKR", format="lua_additive_table", handle-unlocalized="comment")@
+elseif locale == 'ptBR' then
+--@localization(locale="ptBR", format="lua_additive_table", handle-unlocalized="comment")@
+elseif locale == 'ruRU' then
+--@localization(locale="ruRU", format="lua_additive_table", handle-unlocalized="comment")@
 end
